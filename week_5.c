ebdc36b120efb8ae65f91ae451ea7d9e61c90039
Yaşar Kaki <yasarkaki@posta.mu.edu.tr>
2024-12-19 14:08:39 +0300

    .

--- week_5.c
+++ week_5.c
@@ -2,7 +2,7 @@
 
 int main() {
     int numbers[5] = {10, 20, 30, 40, 50};
-    printf("First element: %d\n", numbers[0]); 
+    printf("ilk eleman: %d\n", numbers[0]); 
     return 0;
 }
 
@@ -10,7 +10,7 @@ int main() {
     int numbers[5] = {10, 20, 30, 40, 50};
 
     for (int i = 0; i < 5; i++) {
-        printf("Element at index %d: %d\n", i, numbers[i]);
+        printf("indexteki eleman %d: %d\n", i, numbers[i]);
     }
 
     return 0;
