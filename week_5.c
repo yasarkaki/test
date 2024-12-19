c93a945506ac50d82eb61824495cdcfe7b761c99
Yaşar Kaki <yasarkaki@posta.mu.edu.tr>
2024-12-19 14:10:57 +0300

    .

--- week_5.c
+++ week_5.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    int numbers[5] = {10, 20, 30, 40, 50};
-    printf("ilk eleman: %d\n", numbers[0]); 
+    int sayilar[5] = {10, 20, 30, 40, 50};
+    printf("ilk eleman: %d\n", sayilar[0]); 
     return 0;
 }
 
 int main() {
-    int numbers[5] = {10, 20, 30, 40, 50};
+    int sayilar[5] = {10, 20, 30, 40, 50};
 
     for (int i = 0; i < 5; i++) {
-        printf("indexteki eleman %d: %d\n", i, numbers[i]);
+        printf("indexteki eleman %d: %d\n", i, sayilar[i]);
     }
 
     return 0;
