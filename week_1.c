cd153447dae6646de37f6065e1031ebe97aeb738
Yaşar Kaki <yasarkaki@posta.mu.edu.tr>
2024-12-19 13:33:06 +0300

    last commit

---
+++ week_1.c
@@ -0,0 +1,10 @@
+
+#include <stdio.h>
+
+int main() {
+
+    printf("Hello, Mars!\n");
+    printf("Hello C");
+
+    return 0;
+}
---
+++ week_4.c
@@ -0,0 +1,43 @@
+// #include <stdio.h>
+
+// int main() {
+  
+//   int day = 1;
+
+//     switch (day) {
+//         case 1:
+//             printf("Monday\n");
+//             break;
+//         case 2:
+//             printf("Tuesday\n");
+//             break;
+//         case 3:
+//             printf("Wednesday\n");
+//             break;
+//         default:
+//             printf("Invalid day\n");
+//     }
+//     return 0;
+// }
+
+#include <stdio.h>
+
+int main() {
+  
+  int day = 1;
+
+    switch (day) {
+        case 1:
+            printf("Monday\n");
+            break;
+        case 2:
+            printf("Tuesday\n");
+            break;
+        case 3:
+            printf("Wednesday\n");
+            break;
+        default:
+            printf("Invalid day\n");
+    }
+    return 0;
+}
